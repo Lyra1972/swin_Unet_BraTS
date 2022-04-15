b507c22b89a0b189f64818a1c61438c3f093e23b
Lyra <laaochuanl@gmail.com>
2022-04-16 02:02:24 +1000

    update

--- datasets/dataset_BraTS.py
+++ datasets/dataset_BraTS.py
@@ -19,7 +19,7 @@ def random_rot_flip(image, label):
 
 
 def random_rotate(image, label):
-    angle = np.random.randint(-20, 20)
+    angle = np.random.randint(-25, 25)
     image = ndimage.rotate(image, angle, order=0, reshape=False)
     label = ndimage.rotate(label, angle, order=0, reshape=False)
     return image, label
@@ -32,21 +32,23 @@ class RandomGenerator(object):
     def __call__(self, sample):
         image, label = sample['image'], sample['label']
 
+        image, label = random_rotate(image, label)
         if random.random() > 0.5:
             image, label = random_rot_flip(image, label)
-        elif random.random() > 0.5:
-            image, label = random_rotate(image, label)
+            
         x, y = image.shape
         if x != self.output_size[0] or y != self.output_size[1]:
-            image = zoom(image, (self.output_size[0] / x, self.output_size[1] / y), order=3)  # why not 3?
+            # order 0 = Nearest neighbor upsampling, fill with neighbor's value
+            # order 1 = Nearest bilinear upsampling, order 2 = cubic
+            image = zoom(image, (self.output_size[0] / x, self.output_size[1] / y), order=3)  
             label = zoom(label, (self.output_size[0] / x, self.output_size[1] / y), order=0)
-        image = torch.from_numpy(image.astype(np.float32)).unsqueeze(0)
+        image = torch.from_numpy(image.astype(np.float32)).unsqueeze(0)   # expand image to (1, x, y)
         label = torch.from_numpy(label.astype(np.float32))
         sample = {'image': image, 'label': label.long()}
         return sample
 
 
-class Synapse_dataset(Dataset):
+class BraTS_dataset(Dataset):
     def __init__(self, base_dir, list_dir, split, transform=None):   # list_dir = 'lists/list_BraTS/'
         self.transform = transform  # using transform in torch!
         self.split = split
@@ -63,8 +65,8 @@ class Synapse_dataset(Dataset):
             data = np.load(data_path)
             image, label = data['image'], data['label']
         else:
-            vol_name = self.sample_list[idx].strip('\n')
-            filepath = os.path.join(self.data_dir, slice_name+'.npz')
+            case_name = self.sample_list[idx].strip('\n')
+            data_path = os.path.join(self.data_dir, case_name+'.npz')
             data = np.load(data_path)
             image, label = data['image'], data['label']
 
